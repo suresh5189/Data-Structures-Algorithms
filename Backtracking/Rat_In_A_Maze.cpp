c882783e8304335b614d8606f6c5ea919717057b
suresh5189 <suru242000@gmail.com>
2024-05-03 18:20:13 +0530

    Backtracking File Added

---
+++ Backtracking/Rat_In_A_Maze.cpp
@@ -0,0 +1,109 @@
+// Consider a rat placed at (0, 0) in a square matrix of order N * N. It has to reach the destination at (N - 1, N - 1). Find all possible paths that the rat can take to reach from source to destination. The directions in which the rat can move are 'U'(up), 'D'(down), 'L' (left), 'R' (right). Value 0 at a cell in the matrix represents that it is blocked and rat cannot move to it while value 1 at a cell in the matrix represents that rat can be travel through it.
+// Note: In a path, no cell can be visited more than one time. If the source cell is 0, the rat cannot move to any other cell.
+
+// Example 1:
+
+// Input:
+// N = 4
+// m[][] = {{1, 0, 0, 0},
+//          {1, 1, 0, 1},
+//          {1, 1, 0, 0},
+//          {0, 1, 1, 1}}
+// Output: DDRDRR DRDDRR
+
+// Explanation:
+// The rat can reach the destination at
+// (3, 3) from (0, 0) by two paths - DRDDRR
+// and DDRDRR, when printed in sorted order
+// we get DDRDRR DRDDRR.
+
+// Example 2:
+// Input:
+// N = 2
+// m[][] = {{1, 0},
+//          {1, 0}}
+// Output: -1
+// Explanation:
+// No path exists and destination cell is
+// blocked.
+
+// Your Task:
+// You don't need to read input or print anything. Complete the function printPath() which takes N and 2D array m[ ][ ] as input parameters and returns the list of paths in lexicographically increasing order.
+// Note: In case of no path, return an empty list. The driver will output "-1" automatically.
+
+// Expected Time Complexity: O((3N^2)).
+// Expected Auxiliary Space: O(L * X), L = length of the path, X = number of paths.
+
+// Constraints:
+// 2 ≤ N ≤ 5
+// 0 ≤ m[i][j] ≤ 1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int i, int j, vector<vector<int>> &m, int n, vector<string> &ans, string move, vector<vector<int>> &vis, int di[], int dj[])
+{
+    if (i == n - 1 && j == n - 1)
+    {
+        ans.push_back(move);
+        return;
+    }
+    string dir = "DLRU";
+    for (int ind = 0; ind < 4; ind++)
+    {
+        int nexti = i + di[ind];
+        int nextj = j + dj[ind];
+        if (nexti >= 0 && nextj >= 0 && nexti < n && nextj < n && !vis[nexti][nextj] && m[nexti][nextj] == 1)
+        {
+            vis[i][j] = 1;
+            solve(nexti, nextj, m, n, ans, move + dir[ind], vis, di, dj);
+            vis[i][j] = 0;
+        }
+    }
+}
+
+vector<string> findPath(vector<vector<int>> &m, int n)
+{
+    vector<string> ans;
+    vector<vector<int>> vis(n, vector<int>(n, 0));
+    int di[] = {1, 0, 0, -1};
+    int dj[] = {0, -1, 1, 0};
+    if (m[0][0] == 1)
+        solve(0, 0, m, n, ans, "", vis, di, dj);
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the Size of Cell :";
+    cin >> n;
+    vector<vector<int>> m(n, vector<int>(n));
+    cout << "Enter the Cell Value :" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> m[i][j];
+        }
+    }
+    // ---------------------------------------------
+    // Find Path Function
+    vector<string> ans = findPath(m, n);
+    // ---------------------------------------------
+    // Output
+    cout << "Output : ";
+    if (ans.size() == 0)
+    {
+        cout << -1;
+    }
+    else
+    {
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
+        }
+    }
+    // -------------------------------------------
+    return 0;
+}
